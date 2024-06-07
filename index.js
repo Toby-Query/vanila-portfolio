f2646c51cb81e49d334a1380fc36d8ec5728d756
Toby-Query <142913589+Toby-Query@users.noreply.github.com>
2024-06-07 13:46:29 +0000

    it

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000; // You can change the port number if needed
+const port = process.env.PORT || 3000; // You can change the port number if needed
 
 // Serve static files from the "public" directory
 app.use(express.static('.'));
