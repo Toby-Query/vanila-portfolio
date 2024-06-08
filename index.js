48183cfc298880dd44b399e427643fecdd030221
Toby-Query <142913589+Toby-Query@users.noreply.github.com>
2024-06-08 14:35:00 +0000

    change to index

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static('.'));
 
 // Route to serve home.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'home.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
